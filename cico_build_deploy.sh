ced19c8a120585e756b1da9c7686149aba7e665a
Jaime Melis <j.melis@gmail.com>
2018-07-03 10:53:26 +0200

    Fix path of the image in quay (#33)

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -13,11 +13,11 @@ fi
 docker build -t f8osoproxy-deploy -f "${DOCKERFILE_DEPLOY}" .
 
 if [ "$TARGET" = "rhel" ]; then
-    tag_push ${REGISTRY}/rhel-fabric8-services-fabric8-oso-proxy:$TAG
-    tag_push ${REGISTRY}/rhel-fabric8-services-fabric8-oso-proxy:latest
+    tag_push ${REGISTRY}/openshiftio/rhel-fabric8-services-fabric8-oso-proxy:$TAG
+    tag_push ${REGISTRY}/openshiftio/rhel-fabric8-services-fabric8-oso-proxy:latest
 else
-    tag_push ${REGISTRY}/fabric8-services-fabric8-oso-proxy:$TAG
-    tag_push ${REGISTRY}/fabric8-services-fabric8-oso-proxy:latest
+    tag_push ${REGISTRY}/openshiftio/fabric8-services-fabric8-oso-proxy:$TAG
+    tag_push ${REGISTRY}/openshiftio/fabric8-services-fabric8-oso-proxy:latest
 fi
 
 echo 'CICO: Image pushed, ready to update deployed app'
