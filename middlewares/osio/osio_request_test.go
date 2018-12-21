f2bf6fb4ee00caefd9e23a44e7e6ebdafa1f68e1
nurali-techie <nurali.techie@gmail.com>
2018-12-21 17:43:58 +0530

    minor change, use server incoming req in test

--- middlewares/osio/osio_request_test.go
+++ middlewares/osio/osio_request_test.go
@@ -2,16 +2,15 @@ package osio
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestProcessAuthParam(t *testing.T) {
-	url := "http://osoproxy.com?watch=true&access_token=abcd1234"
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	require.NoError(t, err)
+	target := "/test?watch=true&access_token=abcd1234"
+	req := httptest.NewRequest(http.MethodGet, target, nil)
 
 	processAuthParam(req)
 
@@ -19,4 +18,5 @@ func TestProcessAuthParam(t *testing.T) {
 	assert.Equal(t, "Bearer abcd1234", authHeader)
 	assert.Empty(t, req.URL.Query().Get("access_token"))
 	assert.Equal(t, "true", req.URL.Query().Get("watch"))
+	assert.Equal(t, "/test?watch=true", req.RequestURI)
 }
