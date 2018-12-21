359df06b77cf144bd1b754e7931b373a3cdb1fd3
nurali-techie <nurali.techie@gmail.com>
2018-12-21 17:53:47 +0530

    new test added

--- middlewares/osio/osio_request_test.go
+++ middlewares/osio/osio_request_test.go
@@ -9,14 +9,27 @@ import (
 )
 
 func TestProcessAuthParam(t *testing.T) {
-	target := "/test?watch=true&access_token=abcd1234"
-	req := httptest.NewRequest(http.MethodGet, target, nil)
+	t.Run("ok", func(t *testing.T) {
+		target := "/test?watch=true&access_token=abcd1234"
+		req := httptest.NewRequest(http.MethodGet, target, nil)
 
-	processAuthParam(req)
+		processAuthParam(req)
 
-	authHeader := req.Header.Get("Authorization")
-	assert.Equal(t, "Bearer abcd1234", authHeader)
-	assert.Empty(t, req.URL.Query().Get("access_token"))
-	assert.Equal(t, "true", req.URL.Query().Get("watch"))
-	assert.Equal(t, "/test?watch=true", req.RequestURI)
+		authHeader := req.Header.Get("Authorization")
+		assert.Equal(t, "Bearer abcd1234", authHeader)
+		assert.Empty(t, req.URL.Query().Get("access_token"))
+		assert.Equal(t, "true", req.URL.Query().Get("watch"))
+		assert.Equal(t, "/test?watch=true", req.RequestURI)
+	})
+
+	t.Run("not_set", func(t *testing.T) {
+		target := "/test?watch=true"
+		req := httptest.NewRequest(http.MethodGet, target, nil)
+
+		processAuthParam(req)
+
+		assert.Empty(t, req.Header.Get("Authorization"))
+		assert.Equal(t, "true", req.URL.Query().Get("watch"))
+		assert.Equal(t, "/test?watch=true", req.RequestURI)
+	})
 }
